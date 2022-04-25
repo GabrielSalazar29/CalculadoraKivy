d3e5d5848e5aa707f11b5702e0fa560e12823a5f
Gabriel Salazar <gsaalcantara@gmail.com>
2022-04-25 18:00:36 -0300

    correção de bugs

--- Model/myscreen.py
+++ Model/myscreen.py
@@ -19,17 +19,27 @@ class MyScreenModel:
 
     @x.setter
     def x(self, valor):
-
+        valor = str(valor)
         if self._operador == "":
             if self._x == "":
-                self._x = valor
+                if valor == ".":
+                    self._x = "0."
+                else:
+                    self._x = valor
             else:
-                self._x = str(self._x) + str(valor)
+                if "." in self._x and valor == ".":
+                    valor = ""
+                self._x = self._x + valor
         else:
             if self._y == "":
-                self._y = valor
+                if valor == ".":
+                    self._y = "0."
+                else:
+                    self._y = valor
             else:
-                self._y = str(self._y) + str(valor)
+                if "." in self._y and valor == ".":
+                    valor = ""
+                self._y = self._y + valor
 
     @operador.setter
     def operador(self, valor):
@@ -37,15 +47,28 @@ class MyScreenModel:
 
     def resultado(self):
 
-        if "." in str(self._x):
-            float(self._x)
-        else:
-            int(self._x)
+        self._x = str(self._x)
+        self._y = str(self._y)
 
-        if "." in str(self._y):
-            float(self._y)
+        if len(self._x) >= 2:
+            if self._x[-1] == "0" and self._x[-2] == ".":
+                self._x = int(self._x[:-2])
+            elif "." in self._x:
+                self._x = float(self._x)
+            else:
+                self._x = int(self._x)
         else:
-            int(self._y)
+            self._x = int(self._x)
+
+        if len(self._y) >= 2:
+            if self._y[-1] == "0" and self._y[-2] == ".":
+                self._y = int(self._y[:-2])
+            elif "." in str(self._y):
+                self._y = float(self._y)
+            else:
+                self._y = int(self._y)
+        else:
+            self._y = int(self._y)
 
         if self._operador == "+":
             self._resultado = self._x + self._y
